5c72f20d428171caa0c4df1a4b07ba9bbff11ab7
SmartDev116 <williambennett116ok@proton.me>
2024-03-19 11:54:32 -0600

    fix the component's routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <WalletContextProvider>
         <Routes>
           <Route path="/" element={<Home />} />
+          
           <Route path="/airdrop" element={<AirDrop />} />
         </Routes>
         </WalletContextProvider>
